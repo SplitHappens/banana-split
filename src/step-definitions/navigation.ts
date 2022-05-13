0109bcf2353c9964232edbd0027a0752121b2e50
Cameron Bradley <cpbradley@bunnings.com.au>
2022-05-13 12:27:48 +1000

    bump package version 1.0.2

--- src/step-definitions/navigation.ts
+++ src/step-definitions/navigation.ts
@@ -9,25 +9,25 @@ import { waitFor } from '../support/wait-for-behavior'
 import { PageId } from '../env/global'
 import {logger} from "../logger"
 
-// Given(
-//     /^I am on the "([^"]*)" page$/,
-//     async function(this: ScenarioWorld, pageId: PageId) {
-//         const {
-//             screen: { page },
-//             globalConfig,
-//         } = this
-//
-//         logger.log(`I am on the ${pageId} page`)
-//
-//         await navigateToPage(page, pageId, globalConfig)
-//
-//         await waitFor(() => currentPathMatchesPageId(page, pageId, globalConfig), globalConfig, {
-//             target: pageId,
-//             type: 'page'
-//         })
-//
-//     }
-// )
+Given(
+    /^I am on the "([^"]*)" page$/,
+    async function(this: ScenarioWorld, pageId: PageId) {
+        const {
+            screen: { page },
+            globalConfig,
+        } = this
+
+        logger.log(`I am on the ${pageId} page`)
+
+        await navigateToPage(page, pageId, globalConfig)
+
+        await waitFor(() => currentPathMatchesPageId(page, pageId, globalConfig), globalConfig, {
+            target: pageId,
+            type: 'page'
+        })
+
+    }
+)
 
 Given(
     /^I am directed to the "([^"]*)" page$/,
